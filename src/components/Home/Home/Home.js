9978dfcf1075f0e48efaa15a1ff4801700646d24
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-07 19:03:24 +0600

    hero-area added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import './Home.css';
-import heroImg from '../../../image/hero-img.png'
+import React from "react";
+import "./Home.css";
+import heroImg from "../../../image/hero-img.png";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="container-fluid hero-area">
-                <div className="row py-5 align-items-center">
-                    <div className="col-6 hero-text">
-                        <h1>WE PROUDLY TEACHING ONLINE COURSES WITH ONLINE EXPERTS.</h1>
-                    </div>
-                    <div className="col-6">
-                        <img src={heroImg} className='hero-img img-fluid' alt="" />
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="container-fluid hero-area">
+        <div className="container">
+          <div className="row py-5 justify-content-between align-items-center">
+            <div className="col-12 col-lg-6 hero-text">
+              <h1>WE PROUDLY TEACHING ONLINE COURSES WITH ONLINE EXPERTS.</h1>
+              <button className='hero-btn'>Get Started</button>
             </div>
+            <div className="d-none d-lg-block col-lg-6 text-center">
+              <img src={heroImg} className="hero-img img-fluid" alt="" />
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
