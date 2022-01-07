87bb087cb48c67dd76cbc12c9d8afbf456c4c83c
Md-Tanvir <contact.mdtanvirahmed@gmail.com>
2022-01-07 23:03:20 +0600

    react rating installed

--- src/components/Home/Reviews/Reviews.js
+++ src/components/Home/Reviews/Reviews.js
@@ -14,10 +14,9 @@ const Reviews = () => {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-      
-        speed: 2000,
+        autoplay: true,
         autoplaySpeed: 2000,
-        cssEase: "linear",
+        pauseOnHover: true,
         responsive: [
           {
             breakpoint: 1024,
